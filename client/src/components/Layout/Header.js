7ccb8e4db59cd01411d0f12be9039af3a74f9e95
Krishna Devashish <itskd17@gmail.com>
2023-04-03 22:17:46 +0530

    Initial Commit

---
+++ client/src/components/Layout/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <Link className="navbar-brand" to="/">Expense Manager</Link>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/user">User</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Link</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link disabled">Disabled</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
